1310801918fd823735810893604c707a1315ee8a
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2024-03-03 07:12:54 +0200

    Initialize project files and configuration

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
+
+function App() {
+  return <main className="h-screen my-8 flex gap-8"></main>;
+}
+
+export default App;
